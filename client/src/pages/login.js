8a76c642047c233db684dcd30d806b6c1aad0b1d
Unclejeremy <akpeghaghaoghenevwede@gmail.com>
2023-05-13 21:35:24 +0100

    new modifications

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -6,20 +6,41 @@ const Login = () => {
 
 const loginFormStyle = {
     width: "400px",
-    border: "1px solid red",
+    border: "3px solid blue",
     display: "flex",
     flexDirection: "column",
     alignItems: "Center",
+}
+const inputStyle = {
+  width: "200px",
+  height: "30px",
+  marginBottom: "10px",
+  border: "2px solid blue",
+  borderRadius: "15px",
+  paddingLeft: "5px",
+}
+
+const buttonStyle = {
+  width: "150px",
+  height: "40px",
+  marginBottom: "10px",
+  border: "2px solid blue",
+  borderRadius: "15px",
+  paddingLeft: "5px",
+  color: "white",
+  fontWeight: "bold",
+  backgroundColor: "grey",
+  
 }
   return (
     <main>
         <form style={loginFormStyle}>
         <label for="email">email</label>
         
-        <input name="email" type="email" placeholder="Enter your email" required></input>
+        <input style={inputStyle} name="email" type="email" placeholder="Enter your email" required></input>
         <label for="password">password</label>
-        <input name="password" type="password" placeholder="" required></input>
-        <button type="submit">LOGIN</button>
+        <input style={inputStyle} name="password" type="password" placeholder="" required></input>
+        <button style={buttonStyle} type="submit">LOGIN</button>
         </form>
     </main>
   )
--- client/src/pages/signUp.js
+++ client/src/pages/signUp.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+//import {FaEye, FaEyeSlash} from "react-icons/fa"
 
 
 const SignUp = () => {
@@ -8,20 +9,68 @@ const SignUp = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [lastname, setLastname] = useState("");
   const [email, setEmail] = useState("");
+  const [isRevealPwd, setIsRevealPwd] = useState(false);
 
+  // const handleChange = (e) => {
+  //   setEmail(e.target.value);
+  //   console.log(email)
+  //   }
+const registerNewUser = async (event)  => {
+  event.preventDefault();
+  const response = await fetch("http://localhost:6000/signup", {
+    method: "POST",
+    body: JSON.stringify({firstname, lastname, email, password, phoneNumber}),
+    headers: {"Content-Type": "application/json"},
+  });
+
+  if (response.status === 200) {
+    setFirstname("");
+    setLastname("");
+    setEmail("");
+    setPassword("");
+    setPhoneNumber("");
+    alert("registration successful!");
+    //setRedirect(!redirect);
+  } else {
+    alert("registration failed!")
+  }
+}
+
+
+
+  const SignupFormStyle = {
+    width: "400px",
+    border: "2px solid blue",
+    borderRadius: "20px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "Center",
+    position: "relative",
+    left: "450px",
+    top: "100px",
+    //Colour: "green",
+}
   return (
-    <main onsubmit="alert('Successful submission')" style={{display: "inline-block", border: "2px solid blue", height: "400px", width: "200px", alignItems: "center" }}>
-        <form>
+    <main >
+        <form onSubmit={registerNewUser} style={SignupFormStyle}>
         <label for="firstname">firstname</label>
-        <input name="firstname" type="text" placeholder="Enter your firstname" required></input>
+        <input value={firstname} onChange={(e) => setFirstname(e.target.value)} name="firstname" type="text" placeholder="Enter your firstname" required></input>
         <label for="lastname">lastname</label>
-        <input name="lastName" type="text" placeholder="Enter your lastname" required></input>
+        <input value={lastname} onChange={(e) => setLastname(e.target.value)} name="lastName" type="text" placeholder="Enter your lastname" required></input>
         <label for="email">email</label>
-        <input name="email" type="email" placeholder="Enter your email" required></input>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} name="email" type="email" placeholder="Enter your email" required></input>
         <label for="phonenumber">phonenumber</label>
-        <input name="phonenumber" type="number" placeholder="Enter your phone number" required></input>
+        <input value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} name="phonenumber" type="tel" placeholder="Enter your phone number" required></input>
         <label for="password">password</label>
-        <input name="password" type="password" placeholder="" required></input>
+        <input value={password} onChange={(e) => setPassword(e.target.value)} name="password" type={isRevealPwd ? "text" : "password"} placeholder="" required></input>
+        {/* <span style={{marginLeft: "10px"}}
+              title={isRevealPwd ? "Hide password" : "Show"}
+              onClick={() => setIsRevealPwd(prevState => !prevState) }
+            >
+                {
+                  isRevealPwd ? <FaEyeSlash /> : <FaEye />
+                }
+            </span> */}
         <button type="submit" value="Submit">SIGN UP</button>
         </form>
     </main>
